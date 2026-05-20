f3357eaebb21749446699244a32241021570ef40
ketellynfernadabelter-sys <ketellynfernada.belter@gmail.com>
2026-05-20 11:41:58 -0300

    Add files via upload

---
+++ Modulo 2/Manipulação de Arquivos e Automação/altomacao.py
@@ -0,0 +1,28 @@
+import pyautogui as pg 
+
+#pg.mouseinfo()
+
+
+pg.press("win")
+pg.sleep(1)
+
+pg.write("chrome", interval=0.5)
+
+pg.press("enter")
+
+pg.sleep(1)
+pg.write("www.youtube.com")
+
+pg.press("enter")
+
+pg.sleep(1)
+pg.moveTo(745,112, duration=0.5)
+
+pg.click()
+
+pg.wriler("segredo - kayblack")
+pg.press("enter")
+
+pg.moveTo(745,412, duration=0.5)
+
+
